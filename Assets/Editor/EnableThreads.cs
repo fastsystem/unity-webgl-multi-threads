02f2551e64168aa5498225ac6ade97d7abf44527
fastsystem <info@fast-system.jp>
2019-04-22 19:33:22 +0900

    init

---
+++ Assets/Editor/EnableThreads.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+class EnableThreads
+{
+    static EnableThreads()
+    {
+        PlayerSettings.WebGL.linkerTarget = WebGLLinkerTarget.Wasm;
+        PlayerSettings.WebGL.threadsSupport = true;
+        PlayerSettings.WebGL.memorySize = 512;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Link
+
+[Web Site](https://www.fast-system.jp/)
+
+[Demo](https://www.fast-system.jp/wp-content/uploads/static/unity-create-mesh-cube-excample/)
